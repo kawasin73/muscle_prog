0feacc3ae4452cebfb5986bd51a634ce65e8581e
Kawamura Shintaro <kawasin73@yahoo.co.jp>
2014-12-19 16:52:06 +0900

    mockup ver1

---
+++ js/muscle.js
@@ -0,0 +1,23 @@
+var height;
+var weight;
+var taishiboritu;
+var age;
+var sex;
+var position;
+var goal;
+
+
+$(function(){
+
+});
+
+function getAllValues(){
+	if (!isNaN(document.form1.height)) {
+		height = document.form1.height;
+	} else {
+
+	}
+	
+
+}
+
